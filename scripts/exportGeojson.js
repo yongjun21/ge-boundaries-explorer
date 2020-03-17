0358372283fd6d214f77928cfffb3f1d3962c51b
Yong Jun <yjthong.2009@smu.edu.sg>
2020-03-17 16:12:06 +0800

    Example for Joseph

--- scripts/exportGeojson.js
+++ scripts/exportGeojson.js
@@ -1,7 +1,18 @@
 const fs = require('fs')
+const path = require('path')
 const geojson2svg = require('./geojson2svg')
 
-const data = require('../data/processed/geojson/2020.json').features
+const background = require('../data/processed/geojson/2020.json').features
+
+const grid = fs.readFileSync(path.join(__dirname, '../data/processed/grid/fill.jsonl'), 'utf-8').split('\n').map(line => JSON.parse(line))
+
+const gridSubset = grid.filter(f => {
+  const props = f.properties
+  return props['GE 2020_constituency'] !== props['GE 2015_constituency'] ||
+    props['GE 2020_grc'] !== props['GE 2015_grc']
+}).map(f => Object.assign({}, f, {properties: {}}))
+
+const data = background.concat(gridSubset)
 
 const svg = geojson2svg(data, 2400, 1600, 100)
 
