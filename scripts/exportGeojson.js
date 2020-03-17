3d6ea64785c784cd0a2d3e72286fa50c0894408c
Yong Jun <yjthong.2009@smu.edu.sg>
2020-03-17 17:59:56 +0800

    Generate SVGs

--- scripts/exportGeojson.js
+++ scripts/exportGeojson.js
@@ -2,18 +2,39 @@ const fs = require('fs')
 const path = require('path')
 const geojson2svg = require('./geojson2svg')
 
-const background = require('../data/processed/geojson/2020.json').features
+const {YEARS} = require('./constants')
 
 const grid = fs.readFileSync(path.join(__dirname, '../data/processed/grid/fill.jsonl'), 'utf-8').split('\n').map(line => JSON.parse(line))
 
-const gridSubset = grid.filter(f => {
+for (let i = 1; i < YEARS.length; i++) {
+  const prevElection = 'GE ' + YEARS[i - 1]
+  const currElection = 'GE ' + YEARS[i]
+  grid.forEach(f => {
+    const props = f.properties
+    if (props[prevElection + '_constituency'] == null) return false
+    if (props[currElection + '_constituency'] == null) return false
+    if (
+      props[currElection + '_constituency'] !== props[prevElection + '_constituency'] ||
+      (props[currElection + '_grc'] === 'SMC') !== (props[prevElection + '_grc'] === 'SMC')
+    ) props[currElection + '_changed'] = 1
+  })
+}
+
+grid.forEach(f => {
   const props = f.properties
-  return props['GE 2020_constituency'] !== props['GE 2015_constituency'] ||
-    props['GE 2020_grc'] !== props['GE 2015_grc']
-}).map(f => Object.assign({}, f, {properties: {}}))
+  let changes = 0
+  YEARS.slice(1).forEach(year => {
+    if (props['GE ' + year + '_changed']) {
+      changes++
+      props['GE ' + year + '_cum_changes'] = changes
+    }
+  })
+})
 
-const data = background.concat(gridSubset)
-
-const svg = geojson2svg(data, 2400, 1600, 100)
-
-fs.writeFileSync('data/svg/2020.svg', svg)
+YEARS.slice(1).forEach(year => {
+  const background = require(`../data/processed/geojson/${year}.json`).features
+  const gridSubset = grid.filter(f => f.properties['GE ' + year + '_changed'])
+    .map(f => Object.assign({}, f, {properties: {changes: f.properties['GE ' + year + '_cum_changes']}}))
+  const svg = geojson2svg(background.concat(gridSubset), 2400, 1600, 100)
+  fs.writeFileSync(`data/svg/changes/${year}.svg`, svg)
+})
